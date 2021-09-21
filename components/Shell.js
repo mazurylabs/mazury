5eab35ca59cf8ca96695c875f05191f3a84ba29b
wkulikowski <wojciechkulikowski@icloud.com>
2021-09-21 22:57:55 +0200

    Add desktop connect wallet

--- components/Shell.js
+++ components/Shell.js
@@ -2,11 +2,91 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import { useState, useEffect } from 'react';
+
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import Web3 from "web3"
+
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Shell(props) {
+
+  const [web3Modal, setWeb3Modal] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [connected, setConnected] = useState(false)
+  const [address, setAddress] = useState("...")
+
+  useEffect(() => {
+
+    checkConnection()
+
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+        }
+      },
+    };
+  
+    const web3Modal = new Web3Modal({
+      cacheProvider: false, // optional
+      providerOptions, // required
+      disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+    });
+
+    setWeb3Modal(web3Modal)
+  }, [])
+
+  useEffect(() => {
+    if(connected){
+      fetchAccountData()
+    }
+  }, [connected])
+
+  async function connectWallet() {
+    const provider = await web3Modal.connect();
+    setProvider(provider)
+    setConnected(true)
+  }
+
+  async function disconnectWallet() {
+    // this doesn't work at all XD
+
+    await web3Modal.clearCachedProvider();
+    setProvider(null)
+    setConnected(false)
+  }
+
+  async function fetchAccountData() {
+    const web3 = new Web3(provider)
+    const accounts = await web3.eth.getAccounts()
+    setAddress(accounts[0])
+  }
+
+  const checkConnection = async () => {
+    // works only for metamask so far
+
+    let web3;
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        setProvider(window.ethereum)
+    } else if (window.web3) {
+        web3 = new Web3(window.web3.currentProvider);
+        setProvider(window.web3.currentProvider)
+    };
+
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    if(accounts.length > 0){
+      setConnected(true)
+    }
+  };
+
   return (
     <div className="bg-gray-800 pb-32">
       <Disclosure as="nav" className="bg-gray-800">
@@ -45,50 +125,53 @@ export default function Shell(props) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="ml-3 relative">
-                        <div>
-                          <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={props.user.imageUrl} alt="" />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {props.userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
+                      {connected
+                      ?
+                        <Menu as="div" className="ml-3 relative">
+                          <div>
+                            <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none">
+                              <p
+                                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-900 bg-gray-100 hover:bg-gray-200 focus:outline-none"
+                              >
+                                {`${address.slice(0, 5)}...${address.slice(-3)}`}
+                              </p>
+                            </Menu.Button>
+                          </div>
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                          >
+                            <Menu.Items className="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                              <Menu.Item key={"disconnect"}>
                                 {({ active }) => (
-                                  <a
-                                    href={item.href}
+                                  <button
+                                    onClick={disconnectWallet}
                                     className={classNames(
                                       active ? 'bg-gray-100' : '',
                                       'block px-4 py-2 text-sm text-gray-700'
                                     )}
                                   >
-                                    {item.name}
-                                  </a>
+                                    {"Disconnect"}
+                                  </button>
                                 )}
                               </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                            </Menu.Items>
+                          </Transition>
+                        </Menu>
+                      :
+                        <button
+                          onClick={connectWallet}
+                          type="button"
+                          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-900 bg-gray-100 hover:bg-gray-200 focus:outline-none"
+                        >
+                          Connect
+                        </button>
+                    }
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
