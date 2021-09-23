d4e19cf97d627b1398eda01ae25c40dfa6c2e523
wkulikowski <wojciechkulikowski@icloud.com>
2021-09-23 21:38:12 +0200

    Adjust scores UI for mobile

--- pages/scores.js
+++ pages/scores.js
@@ -38,10 +38,10 @@ export default function Home() {
       />
       <main className="-mt-32">
         <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row">
-          <div className="bg-white rounded-lg h-96 shadow flex-grow mr-10 overflow-hidden">
+          <div className="bg-white rounded-lg h-96 shadow flex-grow md:mr-10 overflow-y-auto mb-10 md:mb-auto">
             <ReferralList />
           </div>
-          <div className="bg-white rounded-lg h-96 shadow px-5 py-2 sm:px-6 w-96">
+          <div className="bg-white rounded-lg h-96 shadow px-5 py-2 sm:px-6 md:w-80 lg:w-96">
             <ScoresList />
           </div>
         </div>
