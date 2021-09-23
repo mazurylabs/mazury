d4dd20484913b4f6a3f6cd7bb3372f123ea4ffe4
wkulikowski <wojciechkulikowski@icloud.com>
2021-09-23 20:50:51 +0200

    Add a list of received referrals

---
+++ components/ReferralList.js
@@ -0,0 +1,67 @@
+// todo supply with actual, not mockup values
+const people = [
+  {
+    name: 'Account not verified',
+    address: '0xF417ACe7b13c0ef4fcb5548390a450A4B75D3eB3',
+    referred_skills: ["solidity", "react", "rust", "design", "memes"]
+  },
+]
+
+export default function ReferralList() {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden border-b border-gray-200 sm:rounded-t-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Referred you for
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {people.map((person) => (
+                  <tr key={person.address}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 h-10 w-10">
+                        <span className="h-10 w-10 rounded-full overflow-hidden bg-gray-100 mx-auto">
+                          <svg className="h-full w-full text-gray-300 rounded-full" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                          </svg>
+                        </span>
+                        </div>
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">{person.name}</div>
+                          <div className="text-sm text-gray-500">{`${person.address.slice(0, 5)}...${person.address.slice(-3)}`}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {person.referred_skills.map((skill) => (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-gray-800 mr-2">
+                          {skill}
+                        </span>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/scores.js
+++ pages/scores.js
@@ -1,4 +1,5 @@
 import Shell from '../components/Shell'
+import ReferralList from '../components/ReferralList'
 import Head from 'next/head'
 
 const user = {
@@ -32,15 +33,15 @@ export default function Home() {
         navigation={navigation}
         user={user}
         userNavigation={userNavigation}
-        header={"Your scores"}
+        header={"Your referrals & scores"}
       />
       <main className="-mt-32">
-        <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+        <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row">
+          <div className="bg-gray-50 rounded-lg h-96 shadow flex-grow mr-10">
+            <ReferralList />
           </div>
-          {/* /End replace */}
+          <div className="bg-white rounded-lg h-96 shadow px-5 py-6 sm:px-6 w-96">
+          </div>
         </div>
       </main>
     </div>
