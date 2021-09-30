48082f9f9d49b4aab4e85eca8e3f35c0e600699f
wkulikowski <wojciechkulikowski@icloud.com>
2021-09-30 18:34:12 +0200

    Add poaps on refer page

--- components/ReferPanel.js
+++ components/ReferPanel.js
@@ -1,15 +1,34 @@
+import axios from "axios";
+
 import SkillsList from './SkillsList';
+import { useState, useEffect } from 'react';
 
 export default function ReferPanel(props) {
 
+
+  const [poaps, setPoaps] = useState([])
+
+  useEffect(() => {
+    if(props.referralAddress) {
+      fetchPaops()
+    }
+  }, [props.referralAddress])
+
+  async function fetchPaops() {
+    const result = await axios.get(`https://api.poap.xyz/actions/scan/${props.referralAddress}`)
+
+    setPoaps(result.data)
+    console.log(result.data)
+  }
+
   return (
-    <div className="h-full px-5 py-6 md:px-16 max-w-screen-lg mx-auto flex flex-col md:flex-row">
-      <div className="md:w-1/3">
+    <div className="h-full px-5 py-6 lg:px-16 max-w-screen-lg mx-auto flex flex-col md:flex-row">
+      <div className="md:w-1/2 lg:w-1/3 mb-8">
         <div
           className="col-span-1 flex flex-col text-center rounded-lg"
         >
           <div className="flex-1 flex flex-col p-8">
-            <span className="h-32 w-32 rounded-full overflow-hidden bg-gray-100 mx-auto">
+            <span className="h-32 w-32 rounded-full overflow-hidden bg-gray-100 mx-auto shadow">
               <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
@@ -20,8 +39,13 @@ export default function ReferPanel(props) {
             </dl>
           </div>
         </div>
+        <div className="grid grid-cols-4 gap-y-2 gap-x-2 md:w-4/5 mx-auto">
+          {poaps.map(poap => (
+            <img className="rounded-full w-14 h-14 lg:w-12 lg:h-12 shadow mx-auto" src={poap.event.image_url} />
+          ))}
+        </div>
       </div>
-      <div className="md:w-2/3">
+      <div className="md:w-1/2 lg:w-2/3">
         <SkillsList
           provider={props.provider}
           signer={props.signer}
