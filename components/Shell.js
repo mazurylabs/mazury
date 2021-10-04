dc1a4299f95cf464a5a7885231c1265512b5f85b
wkulikowski <wojciechkulikowski@icloud.com>
2021-10-04 16:23:00 +0200

    Add FAQ link

--- components/Shell.js
+++ components/Shell.js
@@ -274,9 +274,10 @@ export default function Shell(props) {
           </>
         )}
       </Disclosure>
-      {/* TODO get rid of referral link */}
+      {/* TODO get rid of referral link & FAQ */}
       <header className="">
-        <p className="text-white py-1 text-xs text-right px-4 sm:px-6 lg:px-8">Your referral link: https://dev.app.mazurylabs.com/refer?address={address}</p>
+        {connected && <p className="text-white py-1 text-xs text-right px-4 sm:px-6 lg:px-8">Your referral link: https://dev.app.mazurylabs.com/refer?address={address}</p>}
+        <a target="_blank" href="https://mercury-elk-8de.notion.site/Mazury-early-testing-FAQ-50f1ffe8dd4a4b92ace8453690c3975c"><p className="text-white w-full py-1 text-xs underline text-right px-4 sm:px-6 lg:px-8">Testing FAQ</p></a>
         <div className="max-w-7xl py-10 mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-white">{props.header}</h1>
         </div>
