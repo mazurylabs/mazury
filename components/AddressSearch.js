979c5d66c27dbde3292ac0bccdbcba0499a0e20b
wkulikowski <wojciechkulikowski@icloud.com>
2021-10-06 12:03:00 +0200

    Style/jsx irrevelevant fixes

--- components/AddressSearch.js
+++ components/AddressSearch.js
@@ -14,7 +14,6 @@ export default function AddressSearch(props) {
           id="search"
           placeholder="ETH address or ENS name"
           spellCheck="false"
-          defaultValue={props.searchedAddress}
           debounceTimeout={500}
           onChange={(e) => props.setSearchedAddress(e.target.value)}
           className="shadow-sm focus:outline-none focus:ring-gray-400 focus:border-gray-400 w-full md:w-96 sm:text-sm border-gray-300 border rounded-md py-2 px-4"
--- components/Shell.js
+++ components/Shell.js
@@ -281,10 +281,10 @@ export default function Shell(props) {
       </Disclosure>
       {/* TODO get rid of referral link & FAQ */}
       <header className="">
-        {props.signer && <p className="text-white py-1 text-xs text-right px-4 sm:px-6 lg:px-8">Your referral link: https://dev.app.mazurylabs.com/refer?address={address}</p>}
-        <a target="_blank" href="https://mercury-elk-8de.notion.site/Mazury-early-testing-FAQ-50f1ffe8dd4a4b92ace8453690c3975c"><p className="text-white w-full py-1 text-xs underline text-right px-4 sm:px-6 lg:px-8">Testing FAQ</p></a>
-        <div className="max-w-7xl py-10 mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-white">{props.header}</h1>
+        <div className="max-w-7xl pb-10 mx-auto px-4 sm:px-6 lg:px-8">
+          {props.signer && <p className="text-white py-1 text-xs text-right">Your referral link: https://dev.app.mazurylabs.com/refer?address={address}</p>}
+          <a target="_blank" href="https://mercury-elk-8de.notion.site/Mazury-early-testing-FAQ-50f1ffe8dd4a4b92ace8453690c3975c"><p className="text-white w-full py-1 text-xs underline text-right">Testing FAQ</p></a>
+          <h1 className="text-3xl pt-10 font-bold text-white">{props.header}</h1>
         </div>
       </header>
     </div>
