a570fedfd676c647a4afb971f570a61fc2d9f01c
wkulikowski <wojciechkulikowski@icloud.com>
2021-11-12 17:38:45 -0500

    Add connected profiles

--- components/DashboardTile.js
+++ components/DashboardTile.js
@@ -5,7 +5,7 @@ export default function DashboardTile(props) {
   return (
     <Link href={props.href}>
       <a>
-        <div className="w-64 h-64 rounded-xl bg-gray-100 p-6 mx-4 shadow-sm">
+        <div className="w-64 h-64 rounded-xl bg-white p-6 mx-4 shadow-sm border">
           <h1 className="text-4xl font-semibold">{props.title}</h1>
         </div>
       </a>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,8 @@
 import { skills } from "../utils/const"
 
 import Shell from '../components/Shell'
-import ReferralList from '../components/ReferralList'
-import ScoresList from '../components/ScoresList'
+
+import PeopleList from '../components/PeopleList'
 import Head from 'next/head'
 
 import axios from "axios";
@@ -19,15 +19,84 @@ const navigation = [
   { name: 'Refer a friend', href: '/refer', current: false },
 ]
 
-export default function Home() {
+export default function Dashboard() {
   
   const { signer } = useContext(web3Context)
   const { userData } = useContext(UserDataContext)
-  const [referrals, setReferrals] = useState([])
-  const [badges, setBadges] = useState([])
-  const [scores, setScores] = useState([])
+  const [people, setPeople] = useState([])
+  const [displayPeople, setDisplayPeople] = useState([])
+  const [startPage, setStartPage] = useState(0)
+  const [endPage, setEndPage] = useState(20)
+  const [prevPageURL, setPrevPageURL] = useState(null)
+  const [nextPageURL, setNextPageURL] = useState(null)
+  const [totalPeopleCount, setTotalPeopleCount] = useState(0)
 
   const header_text = userData.ens_name ? `Welcome back, ${userData.ens_name}` : ``
+
+  useEffect(() => {
+    fetchPeople("", "connected")
+  }, [])
+
+  async function fetchPeople(direction="", query="") {
+
+    let result;
+
+    if(direction == "next") {
+      if(nextPageURL == null) return
+      result = await axios.get(nextPageURL);
+      setStartPage(startPage+20)
+      setEndPage(Math.min(endPage+20, totalPeopleCount))
+    } else if(direction == "prev") {
+      if(prevPageURL == null) return
+      result = await axios.get(prevPageURL);
+      setStartPage(Math.max(startPage-20, 0))
+      setEndPage(Math.max(endPage-20, 20))
+    } else if(query != "") {
+      try{
+        result = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/profiles?empty=false&q=${query}`);
+        setEndPage(Math.min(result.data.count, 20))
+      } catch {
+        console.log("search failed")
+        return
+      }
+    } else {
+      result = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/profiles?empty=false`);
+      setEndPage(Math.min(result.data.count, 20))
+    }
+
+    setPrevPageURL(result.data.previous)
+    setNextPageURL(result.data.next)
+
+    const people = []
+    setTotalPeopleCount(result.data.count)
+    let profile_skills;
+    for (const profile of result.data.results) {
+      
+      profile_skills = []
+      skills.forEach(function (skill, index) {
+        if(profile[skill.easName] > 0){
+          profile_skills.push(
+            {
+              "name": skill.humanName,
+              "score": profile[skill.easName],
+            }
+          )
+        }
+      });
+      people.push(
+        {
+          "address": profile.eth_address,
+          "username": profile.ens_name,
+          "avatar": profile.avatar,
+          "skills": profile_skills,
+          "badges": profile.top_badges
+        }
+      )
+    }
+
+    setPeople(people)
+    setDisplayPeople(people)
+  }
 
   return (
     <div>
@@ -58,6 +127,20 @@ export default function Home() {
             <DashboardTile
               href="/people?q=verified_with_twitter"
               title="Verified with twitter"
+            />
+          </div>
+        </div>
+        <h2 className="text-4xl font-medium max-w-7xl w-full mx-auto pb-4 px-4 sm:px-6 lg:px-8">
+          People in your communities
+        </h2>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="bg-white rounded-lg shadow w-full">
+            <PeopleList
+              startPage={startPage}
+              endPage={endPage}
+              totalPeopleCount={totalPeopleCount}
+              people={displayPeople}
+              fetchPeople={fetchPeople}
             />
           </div>
         </div>
