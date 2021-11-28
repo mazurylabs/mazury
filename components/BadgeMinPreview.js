47aea5cfe6d89bacc0b7a002bb761f8a21996b2b
wojtekwtf <wojciechkulikowski@icloud.com>
2021-11-28 23:34:18 +0100

    Fix badge min images

--- components/BadgeMinPreview.js
+++ components/BadgeMinPreview.js
@@ -6,7 +6,7 @@ export default function BadgeMinPreview(props) {
     <div
       className="py-1 px-2 border rounded-md border-gray-400 flex flex-row items-center"
     >
-      <img className="h-4 w-4 rounded-full mr-2" src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${props.badgeData.badge_type.image}`}/>
+      <img className="h-4 w-4 rounded-full mr-2" src={`${props.badgeData.badge_type.image}`}/>
       <p className="text-xs text-gray-800 font-medium">{props.badgeData.badge_type.title}</p>
     </div>
   )
