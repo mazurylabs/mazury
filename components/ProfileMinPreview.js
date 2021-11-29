405083af47bfb6cb84cfc5a0f3ea97f40eb405ac
wojtekwtf <wojciechkulikowski@icloud.com>
2021-11-29 23:59:45 +0100

    Fix referred by avatar display

--- components/ProfileMinPreview.js
+++ components/ProfileMinPreview.js
@@ -5,7 +5,7 @@ export default function ProfileMinPreview(props) {
   return (
     <Link href={`/people/${props.profileData.eth_address}`}>
       <a>
-        <img className="h-8 w-8 rounded-full shadow-sm" src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${props.profileData.avatar}`}/>
+        <img className="h-8 w-8 rounded-full shadow-sm" src={`${props.profileData.avatar}`}/>
       </a>
     </Link>
   )
